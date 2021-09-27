e151bb9b8972434380303000b735a94c2dce11f4
fr4ncisUCM <francisco.calvo.gonz@gmail.com>
2021-09-27 10:49:37 +0200

    tablero bien

--- P1/src/es/ucm/tp1/view/GamePrinter.java
+++ P1/src/es/ucm/tp1/view/GamePrinter.java
@@ -10,9 +10,9 @@ public class GamePrinter {
 
 	private static final String VERTICAL_DELIMITER = "|";
 
-	private static final String ROAD_BORDER_PATTERN = "═";
+	private static final String ROAD_BORDER_PATTERN = "=";
 
-	private static final String LANE_DELIMITER_PATTERN = "─";
+	private static final String LANE_DELIMITER_PATTERN = "-";
 
 	private static final int CELL_SIZE = 7;
 
